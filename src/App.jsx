fb33efdc51765674003203c583fa4a6a535a2a4d
Makhtar <makhtar2gsm@gmail.com>
2026-01-27 02:32:34 +0000

    path of abouts

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AdminRoute from './components/AdminRoute';
 
 // --- PAGES ---
 const Home = lazy(() => import('./pages/Home')); 
+const About = lazy(() => import('./pages/About'));
 const Login = lazy(() => import('./pages/Login'));
 const ITCurriculum = lazy(() => import('./components/ITCurriculum'));
 const HECCurriculum = lazy(() => import('./components/HECCurriculum'));
@@ -60,6 +61,7 @@ function AnimatedRoutes() {
               
               {/* === VISITEUR & TOUT PUBLIC === */}
               <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+              <Route path="/about" element={<PageWrapper><About /></PageWrapper>} />
               <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
               <Route path="/formation/informatique" element={<PageWrapper><ITCurriculum /></PageWrapper>} />
               <Route path="/formation/hec" element={<PageWrapper><HECCurriculum /></PageWrapper>} />
